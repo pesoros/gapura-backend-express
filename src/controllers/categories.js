97e7b2dd14599ff90df34249a502871d2d3cd41d
bayuyuhartono <bayuyuhartono@gmail.com>
2022-05-27 01:07:15 +0700

    assets

--- src/controllers/categories.js
+++ src/controllers/categories.js
@@ -30,7 +30,7 @@ module.exports = {
                 if (element.background == null) {
                     element.backgroundlink = null
                 } else {
-                    element.backgroundlink = request.get('host')+ '/images/articles/' + element.image.background
+                    element.backgroundlink = request.get('host')+ '/images/categories/' + element.background
                 }
             });
 
@@ -59,7 +59,7 @@ module.exports = {
             if (data.background == null) {
                 data.backgroundlink = null
             } else {
-                data.backgroundlink = request.get('host')+ '/images/articles/' + data.background
+                data.backgroundlink = request.get('host')+ '/images/categories/' + data.background
             }
             
             misc.response(response, 200, false, 'Successfull get single Categories', data, request.originalUrl)
