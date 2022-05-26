674e47b76b93e929381c476d65f0beedefaaf74e
bayuyuhartono <bayuyuhartono@gmail.com>
2022-05-27 00:47:56 +0700

    assets

--- src/controllers/categories.js
+++ src/controllers/categories.js
@@ -59,7 +59,7 @@ module.exports = {
             if (data.background == null) {
                 data.backgroundlink = null
             } else {
-                data.backgroundlink = request.get('host')+ '/images/articles/' + element.image
+                data.backgroundlink = request.get('host')+ '/images/articles/' + data.image
             }
             
             misc.response(response, 200, false, 'Successfull get single Categories', data, request.originalUrl)
