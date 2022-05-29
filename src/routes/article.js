df3e07f2cc6fae12a229f32b459083f443764633
bayuyuhartono <bayuyuhartono@gmail.com>
2022-05-29 22:00:48 +0700

    jwt

--- src/routes/article.js
+++ src/routes/article.js
@@ -6,8 +6,8 @@ const Route = express.Router()
 Route.get('/', Article.getAll)
     .get('/show/:article_id', Article.getSingleArticle)
     .get('/slug/:slug', Article.getSingleArticleSlug)
-    .post('/add', Article.addArticle)
-    .patch('/update', Article.updateArticle)
-    .delete('/delete/:article_id', Article.deleteArticle)
+    .post('/add', jwtCheck.CheckToken, Article.addArticle)
+    .patch('/update', jwtCheck.CheckToken, Article.updateArticle)
+    .delete('/delete/:article_id', jwtCheck.CheckToken, Article.deleteArticle)
 
 module.exports = Route
--- src/routes/footer.js
+++ src/routes/footer.js
@@ -5,7 +5,7 @@ const randomguy = require('../helper/randomguy')
 const Route = express.Router()
 
 Route.get('/', Footer.getAll)
-    .patch('/', Footer.updateFooter)
+    .patch('/', jwtCheck.CheckToken, Footer.updateFooter)
 
 module.exports = Route
  
