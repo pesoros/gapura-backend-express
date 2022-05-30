dedd9f51a6ca73d5d3547b703ee307b64b57ece8
bayuyuhartono <bayuyuhartono@gmail.com>
2022-05-31 00:13:06 +0700

    articlelist

--- src/models/Home.js
+++ src/models/Home.js
@@ -70,9 +70,10 @@ module.exports = {
     },
     getArticlesTerbaru: (categories_id) => {
         return new Promise((resolve, reject) => {
-            const query = `SELECT *
-            FROM articles
+            const query = `SELECT art.*, cat.title as categories
+            FROM articles art, categories cat
             WHERE categories_id = ${categories_id}
+            AND art.categories_id = cat.id
             ORDER BY createdAt DESC`
             connection.query(query, (error, result) => {
                 if (error) {
@@ -85,9 +86,10 @@ module.exports = {
     },
     getArticlesLama: (categories_id,month) => {
         return new Promise((resolve, reject) => {
-            const query = `SELECT *
-            FROM articles
-            WHERE categories_id = ${categories_id} 
+            const query = `SELECT art.*, cat.title as categories
+            FROM articles art, categories cat
+            WHERE categories_id = ${categories_id}
+            AND art.categories_id = cat.id 
             AND createdAt >= DATE_ADD(LAST_DAY(DATE_SUB(NOW(), INTERVAL 2 MONTH)), INTERVAL 1 DAY) 
             AND createdAt <= DATE_SUB(NOW(), INTERVAL 1 MONTH)
             ORDER BY id DESC`
