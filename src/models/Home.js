697dc371896efb067fdc47ca436ca57682c00cf4
bayuyuhartono <bayuyuhartono@gmail.com>
2022-06-08 21:51:00 +0700

    wdglama

--- src/models/Home.js
+++ src/models/Home.js
@@ -90,7 +90,8 @@ module.exports = {
             FROM articles art, categories cat
             WHERE art.categories_id = ${categories_id}
             AND art.categories_id = cat.id 
-            AND art.createdAt <= DATE_SUB(NOW(), INTERVAL 1 MONTH)
+            AND MONTH(rt.createdAt) = MONTH(NOW() - INTERVAL 2 MONTH) 
+            AND (YEAR(rt.createdAt) = YEAR(NOW()) OR YEAR(rt.createdAt) = YEAR(NOW() - INTERVAL 2 MONTH)
             ORDER BY art.id DESC`
             connection.query(query, (error, result) => {
                 if (error) {
